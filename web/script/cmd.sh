da59c621e5ab2f8742e89092230692f08fef2000
stedplay <stedplay@gmail.com>
2020-06-19 13:32:26 +0900

    feat: Wait for DB to be ready for successful wordpress installation.

--- web/script/cmd.sh
+++ web/script/cmd.sh
@@ -8,6 +8,15 @@ if ! wp core is-installed --path=${wordpress_siteurl_path} --allow-root; then
   mkdir -p ${wordpress_siteurl_path}
   cd ${wordpress_siteurl_path}
   wp core download --version=${WORDPRESS_VERSION} --locale=${LOCALE} --allow-root
+  # Wait for DB to be ready.
+  while :
+  do
+    db_ping=$(mysqladmin ping --user=${DB_USER} --password=${DB_PASSWORD} --host=${DB_HOST} 2>&1)
+    if echo "${db_ping}" | grep 'alive'; then
+      break
+    fi
+    sleep 1s
+  done
   # Create 'wp-config.php'.
   wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX} --allow-root
   # Install wordpress.

fcaf5b00353e29bd8c4c6143bcd77f57f093295a
stedplay <stedplay@gmail.com>
2020-06-19 16:33:31 +0900

    feat: Define variable that set whether wordpress is initially installed.

--- web/script/cmd.sh
+++ web/script/cmd.sh
@@ -6,7 +6,7 @@ wordpress_scheme_authority=${WEB_HTML_SCHEME}://${WEB_HTML_FQDN}:${WEB_HTML_PORT
 wordpress_siteurl_url=${wordpress_scheme_authority}${WORDPRESS_SITEURL_DIR}
 wordpress_home_url=${wordpress_scheme_authority}${WORDPRESS_HOME_DIR}
 
-if ! wp core is-installed --path=${wordpress_siteurl_path} --allow-root; then
+if ${WORDPRESS_INITIAL_INSTALL} && ! wp core is-installed --path=${wordpress_siteurl_path} --allow-root; then
   # Download wordpress.
   mkdir -p ${wordpress_siteurl_path}
   cd ${wordpress_siteurl_path}

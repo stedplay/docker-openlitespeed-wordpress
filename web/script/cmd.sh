d3e4f18c69973d69bca60cb128fc76bc2de49ec8
stedplay <stedplay@gmail.com>
2020-06-19 13:06:04 +0900

    feat: Install wordpress.

--- web/script/cmd.sh
+++ web/script/cmd.sh
@@ -1,5 +1,7 @@
 document_root_path=/var/www/vhosts/localhost/html/
 wordpress_siteurl_path=${document_root_path%/}${WORDPRESS_SITEURL_DIR%/}/
+wordpress_scheme_authority=${WEB_HTML_SCHEME}://${WEB_HTML_FQDN}:${WEB_HTML_PORT}
+wordpress_siteurl_url=${wordpress_scheme_authority}${WORDPRESS_SITEURL_DIR}
 
 if ! wp core is-installed --path=${wordpress_siteurl_path} --allow-root; then
   # Download wordpress.
@@ -8,4 +10,6 @@ if ! wp core is-installed --path=${wordpress_siteurl_path} --allow-root; then
   wp core download --version=${WORDPRESS_VERSION} --locale=${LOCALE} --allow-root
   # Create 'wp-config.php'.
   wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX} --allow-root
+  # Install wordpress.
+  wp core install --title=${WORDPRESS_TITLE} --admin_user=${WORDPRESS_USER} --admin_password=${WORDPRESS_PASSWORD} --admin_email=${WORDPRESS_EMAIL} --url=${wordpress_siteurl_url} --allow-root
 fi

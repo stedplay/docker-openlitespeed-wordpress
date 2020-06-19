39e85d410bb3640bd499d1be2675f2a908e051e9
stedplay <stedplay@gmail.com>
2020-06-19 12:52:09 +0900

    feat: Create wp-config.php.

--- web/script/cmd.sh
+++ web/script/cmd.sh
@@ -6,4 +6,6 @@ if ! wp core is-installed --path=${wordpress_siteurl_path} --allow-root; then
   mkdir -p ${wordpress_siteurl_path}
   cd ${wordpress_siteurl_path}
   wp core download --version=${WORDPRESS_VERSION} --locale=${LOCALE} --allow-root
+  # Create 'wp-config.php'.
+  wp core config --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --dbprefix=${DB_PREFIX} --allow-root
 fi
